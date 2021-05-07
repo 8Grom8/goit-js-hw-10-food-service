99d4195279e3ef276196f691d319f7fe3f7fc61e
Michal Lenyuk <mlcorp@ukr.net>
2021-05-07 17:08:57 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import './js/test';
+// import print from './js/test';
+
+// import { text, printText } from './js/test';
+// console.log(print);
+// print('Not hi');
+// console.log(text);
+// printText('JS');
+
+import * as object from './js/test';
+object.printText('JS');
+console.log(object.text);
